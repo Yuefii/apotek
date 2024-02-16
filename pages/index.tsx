b8ebba41f35625ed0d811db8a3c9a41806d58c58
Yuefii <muffidahmadiawan31@gmail.com>
2024-02-16 08:56:32 +0700

    starter project

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HomePages = () => {
+  return (
+    <main>
+      <div className="h-screen flex justify-center items-center">
+        <h1>WELCOME TO APOTEK</h1>
+      </div>
+    </main>
+  );
+};
+
+export default HomePages;
