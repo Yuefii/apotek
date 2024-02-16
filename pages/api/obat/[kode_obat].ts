45202de456f552637da880c010001913d804a864
Yuefii <muffidahmadiawan31@gmail.com>
2024-02-16 09:55:09 +0700

    api: menambahkan crud data obat

---
+++ pages/api/obat/[kode_obat].ts
@@ -0,0 +1,36 @@
+import { PrismaClient } from '@prisma/client'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+const prisma = new PrismaClient()
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { kode_obat }: any = req.query
+  if (req.method === 'GET') {
+    const obat = await prisma.obat.findUnique({
+      where: { kode_obat: kode_obat },
+    })
+    // console.log(obat);
+
+    if (obat) {
+      res.json(obat)
+    } else {
+      res.status(404).json({ message: 'Obat tidak ditemukan' })
+    }
+  } else if (req.method === 'PATCH') {
+    const { ...data } = req.body
+    const updatedObat = await prisma.obat.update({
+      where: { kode_obat: kode_obat },
+      data: data,
+    })
+    // console.log(updatedObat);
+
+    res.json(updatedObat)
+  } else if (req.method === 'DELETE') {
+    const deletedObat = await prisma.obat.delete({
+      where: { kode_obat: kode_obat },
+    })
+    res.json(deletedObat)
+  } else {
+    res.status(405).json({ message: 'Method Not Allowed' })
+  }
+}
---
+++ pages/api/obat/index.ts
@@ -0,0 +1,35 @@
+import { PrismaClient } from '@prisma/client'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+const prisma = new PrismaClient()
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === 'POST') {
+        const { nama_obat, harga_obat, satuan_obat } = req.body
+
+        // Generate kode_obat
+        const lastObat = await prisma.obat.findFirst({
+            orderBy: {
+                kode_obat: 'desc'
+            }
+        })
+        const lastId = lastObat ? parseInt(lastObat.kode_obat.substring(5)) : 0
+        const nextId = lastId + 1
+        const kode_obat = 'KDOBT' + nextId.toString().padStart(5, '0')
+
+        const newObat = await prisma.obat.create({
+            data: {
+                kode_obat,
+                nama_obat,
+                harga_obat,
+                satuan_obat,
+            },
+        })
+        res.json(newObat)
+    } else if (req.method === 'GET') {
+        const allObat = await prisma.obat.findMany()
+        res.json(allObat)
+    } else {
+        res.status(405).json({ message: 'Method Not Allowed' })
+    }
+}
