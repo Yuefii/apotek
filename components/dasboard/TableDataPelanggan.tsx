f4b02511acfb196430e2e840629fd34eee09a4f8
Yuefii <muffidahmadiawan31@gmail.com>
2024-02-20 22:17:08 +0700

    feat: fetch data pelanggan

---
+++ components/dasboard/TableDataPelanggan.tsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import Pagination from "./Pagination";
+
+const TabelDataPelanggan = ({ pelanggan }: any) => {
+  const [activePage, setActivePage] = useState<number>(1);
+  const itemsPerPage = 10;
+
+  const startIndex = (activePage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+
+  const currentData = pelanggan.slice(startIndex, endIndex);
+
+  return (
+    <>
+      <div className="relative overflow-x-auto shadow-md">
+        <table className="w-full text-sm table-auto">
+          <thead className="text-xs text-white uppercase bg-gray-400">
+            <tr className="text-center">
+              <th className="py-3 border-r">No</th>
+              <th className="py-3 border-r">Kode Pelanggan</th>
+              <th className="py-3 border-r">Tanggal Transaksi</th>
+              <th className="py-3 border-r">Total Pembayaran</th>
+              <th className="py-3 border-r">Obat</th>
+            </tr>
+          </thead>
+          <tbody>
+            {currentData.map((item: any, index: any) => (
+              <tr
+                key={startIndex + index}
+                className="text-xs text-center bg-white border-b hover:bg-gray-50"
+              >
+                <td className="py-3 border-r">{startIndex + index}</td>
+                <td className="py-3 border-r">{item.kode_pelanggan}</td>
+                <td className="py-3 border-r">{item.tanggal_transaksi}</td>
+                <td className="py-3 border-r">{item.total_pembayaran}</td>
+                <td className="py-3 border-r text-left px-2">
+                  {item.obat &&
+                    item.obat.map((item: any, index: any) => (
+                      <li key={index}>{item.nama_obat}</li>
+                    ))}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <Pagination
+        totalItems={pelanggan.length}
+        itemsPerPage={itemsPerPage}
+        activePage={activePage}
+        onPageChange={setActivePage}
+      />
+    </>
+  );
+};
+
+export default TabelDataPelanggan;
--- pages/dashboard/data-transaksi/pelanggan/index.tsx
+++ pages/dashboard/data-transaksi/pelanggan/index.tsx
@@ -1,7 +1,29 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import Layout from "@/components/dasboard/Layout"
+import TabelDataPelanggan from "@/components/dasboard/TableDataPelanggan"
+import React, { useEffect, useState } from "react";
 import { FaUserFriends } from "react-icons/fa"
 
 const pelanggan = () => {
+  const [pelanggan, setPelanggan] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const res = await fetch("/api/transaction/pelanggan");
+      if (!res.ok) {
+        throw new Error("Gagal mengambil data obat");
+      }
+      const result = await res.json();
+      setPelanggan(result.data);
+    } catch (error) {
+      console.error("Terjadi kesalahan:", error);
+      setPelanggan([]);
+    }
+  };
   return (
     <>
       <Layout>
@@ -10,6 +32,19 @@ const pelanggan = () => {
             <div className="flex gap-2 items-center">
               <FaUserFriends />
               <h1 className="font-semibold">Pelanggan</h1>
+            </div>
+          </div>
+          <div className="mx-10 mt-5">
+            <div className="flex justify-end items-center">
+              <button
+                onClick={()=> {}}
+                className="bg-blue-600 text-white font-semibold px-2 py-1 text-xs rounded mb-2"
+              >
+                Tambah
+              </button>
+            </div>
+            <div className="container mx-auto">
+              <TabelDataPelanggan pelanggan={pelanggan} />
             </div>
           </div>
         </div>
